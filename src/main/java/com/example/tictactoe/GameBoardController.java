f77cf9abcfd1f2d594fcf7e1a7ea8e49131da989
mtobino <tobino68@students.rowan.edu>
2023-05-29 19:54:05 -0400

    Initial Commit and GUI Start

---
+++ src/main/java/com/example/tictactoe/GameBoardController.java
@@ -0,0 +1,11 @@
+package com.example.tictactoe;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+public class GameBoardController {
+    public BorderPane OuterPane;
+    public GridPane GameGrid;
+    public Label Title;
+}
